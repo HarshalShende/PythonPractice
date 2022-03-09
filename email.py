07d9ff9514ed0f23b3713c5a01e4378e3c71cec7
HarshalShende <harshalshende357@gmail.com>
2022-03-09 12:57:49 +0530

    first commit

---
+++ email.py
@@ -0,0 +1,38 @@
+# g@g.in minimum 6 charecters
+# first charecter not number
+# @ charecter in email and count of @ must be 1
+# position of "." -3 or -4 index
+# search space -- if space found wrong
+# charecter must not be uppercase
+
+
+email = input("Enter Your Email : ")
+k, j, d = 0, 0, 0
+if len(email) >= 6:
+    if email[0].isalpha():
+        if ("@" in email) and (email.count("@") == 1):
+            if (email[-4] == ".") ^ (email[-3] == "."):
+                for i in email:
+                    if i == i.isspace():
+                        k = 1
+                    elif i.isalpha():
+                        if i == i.upper():
+                            j = 1
+                    elif i.isdigit():
+                        continue
+                    elif i == "_" or i == "." or i == "@":
+                        continue
+                    else:
+                        d = 1
+                if k == 1 or j == 1 or d == 1:
+                    print("Wrong Email 5")
+                else:
+                    print("Right Email")
+            else:
+                print("Wrong Email 4")
+        else:
+            print("Wrong Email 3")
+    else:
+        print("Wrong Email 2")
+else:
+    print("Wrong Email 1")
