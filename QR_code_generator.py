c9742b4256310ba098d3d7982520cfb7d12ef7b6
HarshalShende <harshalshende357@gmail.com>
2022-03-09 14:44:57 +0530

    qr code generator

---
+++ QR_code_generator.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img=qr.make("www.techdev360.com")
+img.save("visitsite.png")
---
+++ qrcode_format.py
@@ -0,0 +1,13 @@
+import qrcode
+from PIL import Image
+
+qr=qrcode.QRCode(version=1,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H,
+                 box_size=10,
+                 border=4)
+
+qr.add_data("https://www.hackinginhindi.com/")
+qr.make(fit=True)
+img=qr.make_image(fill_color="red", back_color="white")
+img.save("wscube_web.png")
+
