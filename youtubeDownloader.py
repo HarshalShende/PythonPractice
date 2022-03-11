dcabd54cc6ede64828952a30066ebd338b5dd681
HarshalShende <harshalshende357@gmail.com>
2022-03-11 23:03:02 +0530

    program to get contact number details

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,20 +1,33 @@
-import enum
-from pytube import YouTube
+# import enum
+# from pytube import YouTube
 
-link="https://youtu.be/PlsQisQhumY"
-youtube_1=YouTube(link)
+# link="https://youtu.be/PlsQisQhumY"
+# youtube_1=YouTube(link)
 
-#print(youtube_1.title)
-#print(youtube_1.thumbnail_url)
+# #print(youtube_1.title)
+# #print(youtube_1.thumbnail_url)
 
-videos = youtube_1.streams.all() # all format
-#videos = youtube_1.streams.filter(only_audio=True) # all format
+# videos = youtube_1.streams.all() # all format
+# #videos = youtube_1.streams.filter(only_audio=True) # all format
 
-vid=list(enumerate(videos))
-for i in vid:
-    print(i)
+# vid=list(enumerate(videos))
+# for i in vid:
+#     print(i)
 
-strm=int(input("Enter : "))
-videos[strm].download()
-print("Successfully")
+# strm=int(input("Enter : "))
+# videos[strm].download()
+# print("Successfully")
+
+#playlist Download
+
+from pytube import Playlist
+
+py==Playlist("Playlist URL")
+
+print(f'Downloading :{py.title}')
+
+for video in py.videos:
+    video.streams.first().download()
+
+
 
