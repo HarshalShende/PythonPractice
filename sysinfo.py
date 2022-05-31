1fbf51bd29b97663aee5553b980ca1d10837523d
HarshalShende <harshalshende357@gmail.com>
2022-05-31 06:16:59 +0530

    system info

---
+++ sysinfo.py
@@ -0,0 +1,8 @@
+import platform
+print(f"Node: {platform.node()}")
+print(f"Plateform: {platform.platform()}")
+print(f"System: {platform.system()}")
+print(f"Processor: {platform.processor()}")
+print(f"Architecture: {platform.architecture()}")
+print(f"Version: {platform.version()}")
+print(f"Release: {platform.release()}") 
